7d2570fa93b259ff20871e6596ac837efe707cc6
Adrian Cavazos Tamez <adrian.cavazos@neoris.cxnetworks.net>
2018-07-27 17:13:55 -0500

    miprimer commit

---
+++ src/app/Hero/hero.component.ts
@@ -0,0 +1,11 @@
+// Import component decorator
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+
+// Component class
+export class HeroComponent {}
